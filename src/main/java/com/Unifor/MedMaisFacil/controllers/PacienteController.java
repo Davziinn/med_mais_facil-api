5ca3ef8d19f017a092e83070776f8a9792f6781a
Davziinn <mdavi6085@gmail.com>
2026-04-30 09:42:23 -0300

    corrigindo dados

--- src/main/java/com/Unifor/MedMaisFacil/controllers/PacienteController.java
+++ src/main/java/com/Unifor/MedMaisFacil/controllers/PacienteController.java
@@ -1,7 +1,6 @@
 package com.Unifor.MedMaisFacil.controllers;
 
 import com.Unifor.MedMaisFacil.dtos.paciente.*;
-import com.Unifor.MedMaisFacil.entity.PacienteEntity;
 import com.Unifor.MedMaisFacil.mapper.PacienteMapper;
 import com.Unifor.MedMaisFacil.models.Paciente;
 import com.Unifor.MedMaisFacil.service.PacienteService;
--- src/main/java/com/Unifor/MedMaisFacil/dtos/paciente/PacienteResponseDTO.java
+++ src/main/java/com/Unifor/MedMaisFacil/dtos/paciente/PacienteResponseDTO.java
@@ -1,13 +1,9 @@
 package com.Unifor.MedMaisFacil.dtos.paciente;
 
-import java.time.LocalDateTime;
-
 public record PacienteResponseDTO (
         Long id,
         String nome,
         String cpf,
         int idade,
-        String sexo,
-        LocalDateTime criadoEm,
-        LocalDateTime atualizadoEm
+        String sexo
 ) {}
